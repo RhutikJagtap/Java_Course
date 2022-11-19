9948320266b01727874b4a2a0d42713c6d5f12fc
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-19 19:15:03 +0530

    Create hello.java

---
+++ Introduction to Java/hello.java
@@ -0,0 +1,10 @@
+//first java programme
+class hello
+{
+      //Main Method
+    public static void main(String args[])    
+    {
+        System.out.println("hello java programming");
+    }
+}
+// @Code By Rj
