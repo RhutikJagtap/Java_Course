2dd9f4b2cf9a57b59e9e5b193b169a0aa85f680c
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-22 21:46:22 +0530

    Add files via upload

---
+++ Introduction to Java/nested_if.java
@@ -0,0 +1,18 @@
+// programm for nested if statement
+public class nested_if {
+    public static void main(String args[])
+     {
+        int age = 20;
+        int weight = 80;
+        if (age >= 18)
+         {
+            if (weight > 50)
+             {
+                System.out.println("Eligible to donate blood");
+            }
+        }
+
+    }
+
+}
+// @Code By Rj
