40934c3813b29db4da222da15356e625e03cc0fd
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-22 21:12:42 +0530

    Add files via upload

---
+++ Introduction to Java/bitwise.java
@@ -0,0 +1,19 @@
+// Program for Bitwise Operator
+public class bitwise {
+    public static void main(String args[])
+    {
+        int a=60;
+        int b=20;
+        // Binary AND operator
+        System.out.println("a&b="+(a&b));
+        // Binary OR Operator
+        System.out.println("a|b="+(a|b));
+        // Binary Left Shift Operator
+        System.out.println("a<<2="+(a<<2));
+        // Binary Right Shift Operator
+        System.out.println("a>>2="+(a>>2));
+        // Shift Right zero fill operator
+        System.out.println("a>>>2="+(a>>>2));
+    }
+}
+// @Code By Rj
