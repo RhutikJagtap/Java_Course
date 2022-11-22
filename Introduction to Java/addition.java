b83a7512c1f6cf51e923e93309451e6a18a54a81
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-22 21:47:51 +0530

    Add files via upload

---
+++ Introduction to Java/addition.java
@@ -0,0 +1,11 @@
+//java programm to find addition of two integer
+public class addition 
+{
+    public static void main(String args[])
+    {
+        int a=6,b=10,sum;
+        sum=a+b;
+        System.out.println("Addition="+sum);
+    }
+}
+// @Code By Rj
