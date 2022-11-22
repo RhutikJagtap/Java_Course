220b661a6bfdd89735dc7abbff739800b47943c1
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-22 21:35:28 +0530

    Add files via upload

---
+++ Introduction to Java/even_odd.java
@@ -0,0 +1,18 @@
+//programm to find even or odd number using if else statement
+public class even_odd {
+    public static void main(String args[]) {
+        //definining variable
+        int number=17;
+        if (number%2==0) //check if the number is divisible by 2
+         {
+            System.out.println("Even Number"); 
+        } else
+         {
+            System.out.println("Odd Number");
+            
+        }
+        
+    }
+
+}
+// @Code By Rj
