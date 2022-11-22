4170bee71c92cc9202c8cda0e34f0c58540ff21b
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-22 21:39:42 +0530

    Add files via upload

---
+++ Introduction to Java/rj.java
@@ -0,0 +1,10 @@
+ //java programme to display your name and address
+ class rj 
+ {
+    public static void main(String args[])
+    {
+        System.out.println("Rhutik");
+        System.out.println("jagtapmala kolgaon");
+    }
+}
+// @Code By Rj
