64cf409eff08f3ca07e88421ba838801f534b312
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-22 21:37:54 +0530

    Add files via upload

---
+++ Introduction to Java/if_statement.java
@@ -0,0 +1,12 @@
+// program for if statement
+public class if_statement {
+    public static void main(String args[]) {
+        int number = 25;
+        if (number > 0)
+        {
+            System.out.println("Number is Greater than zero(0)");
+        }
+
+    }
+}
+// @Code By Rj
