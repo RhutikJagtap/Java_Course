6d0d08f09692f143656d99afaccb38e78b895538
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-22 21:25:14 +0530

    Add files via upload

---
+++ Introduction to Java/Area_Of_Circle.java
@@ -0,0 +1,11 @@
+//java programme to find area of circle using final variable
+public class area {
+    public static void main(String args[])
+    {
+        int radius=6;
+        final double PI=3.14;
+        double Area=PI*radius*radius;
+        System.out.println("Area of circle="+Area);
+    }
+    
+}
