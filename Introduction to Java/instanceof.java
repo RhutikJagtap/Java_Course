b450a009930313fdad13f7fe106be3d30c68a51f
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-22 21:33:47 +0530

    Add files via upload

---
+++ Introduction to Java/instanceof.java
@@ -0,0 +1,10 @@
+// Program For instanceof operator
+public class instanceof {
+    public static void main(String args[]) {
+        String name = "Rhutik";
+        // Following will return true since name is type of string
+        boolean result = name instanceof String;
+        System.out.println(result);
+    }
+}
+// @Code By Rj
