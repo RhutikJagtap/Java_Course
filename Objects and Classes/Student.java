2d0162c4b23c92b6aa0333ea7aac69de085bb4f1
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2022-11-27 18:11:26 +0530

    Create Student.java

---
+++ Objects and Classes/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    int id;
+    String name;
+    public static void main(String args[])   //creating main method
+    {
+        // Create an Object of Student
+        Student S1 = new Student();
+        System.out.println("Student id=" + S1.id);
+        // Accessing member through reference variable
+        System.out.println("Student name=" + S1.name);
+    }
+}
+// @Code By Rj
