4e4e5b9f58ab2e46869cdff8fe284ca1c2258cdb
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2023-08-17 20:47:36 +0530

    Add files via upload

---
+++ Pattern Program/pattern2.java
@@ -0,0 +1,23 @@
+public class pattern2 {
+    public static void main(String[] args) {
+        for (int row = 0; row < 10; row++) {
+            for (int column = 0; column < 10; column++) {
+                System.out.print(column + " ");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
+// 0 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4 5 6 7 8 9
+// 0 1 2 3 4 5 6 7 8 9
