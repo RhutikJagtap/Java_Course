820542aaaaffd2922918a8eb2373dd8877332b69
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2023-09-05 07:55:16 +0530

    Create ScannerDemo.java

---
+++ Logic Based Programming/ScannerDemo.java
@@ -0,0 +1,20 @@
+// Q1) Write a Java program to read three numbers as a dynamic input and perform addition operation over the provided numbers?
+
+import java.util.*;
+class Test{
+	public static void main(String[] args){
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("First Value    : ");
+		int num1 = s.nextInt();
+
+		System.out.print("Second Value   : ");
+		int num2 = s.nextInt();
+
+		System.out.print("Third Value    : ");
+		int num3 = s.nextInt();
+
+		System.out.println("Addition     : "+(num1+num2+num3));
+	}
+}
+
