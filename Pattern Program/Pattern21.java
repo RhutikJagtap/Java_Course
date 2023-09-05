ee638f6cee633c94942a0a8a475e71545019226e
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2023-09-05 07:37:36 +0530

    Add files via upload

---
+++ Pattern Program/Pattern21.java
@@ -0,0 +1,32 @@
+class Pattern21 
+{
+	public static void main(String[] args) 
+	{
+		for(int row=0;row<10;row++)
+		{
+			for(int spaces=0;spaces<=(9-row);spaces++)
+			{
+				System.out.print(" "+" ");
+			}
+			for(int column=0;column<=row;column++)
+			{
+				System.out.print((9-column)+" ");
+			}
+			System.out.println();
+
+		}
+	}
+}
+/*
+                    9
+                  9 8
+                9 8 7
+              9 8 7 6
+            9 8 7 6 5
+          9 8 7 6 5 4
+        9 8 7 6 5 4 3
+      9 8 7 6 5 4 3 2
+    9 8 7 6 5 4 3 2 1
+  9 8 7 6 5 4 3 2 1 0
+
+*/
