821dd1f270bc2f44c2198f62b18774467c119fc2
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-02-03 17:40:18 +0530

    Update JDBC1.java

--- JDBC Programs/JDBC1.java
+++ JDBC Programs/JDBC1.java
@@ -5,7 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class JDBC2{
+public class JDBC1{
 
 	String url = "jdbc:mysql://localhost:3306/JDBC1";
 	String username = "root";
