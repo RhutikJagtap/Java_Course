56bfcefa306a03f9ce3bb529f774ca0fe3f558f4
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-02-03 19:17:51 +0530

    Create JDBC2.java

---
+++ JDBC Programs/JDBC2.java
@@ -0,0 +1,45 @@
+//JDBC Program to Update data in table
+package com.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JDBC2 {
+
+	String url = "jdbc:mysql://localhost:3306/JDBC1";
+	String username = "root";
+	String password = "rj@5588";
+
+	public String updateData() {
+
+		// update query
+		String query = "update  student set name='Kunal' where id=1";  // single quote must 'kunal'
+
+		try {
+			// regisater the Driver Class
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// create Connection
+			Connection con = DriverManager.getConnection(url, username, password);
+
+			// create Statement
+			Statement st = con.createStatement();
+
+      // execute query
+			st.executeUpdate(query);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "data updated Successfully";
+	}
+
+	public static void main(String[] args) {
+		JDBC2 jdbc2 = new JDBC2();
+		String msg = jdbc2.updateData();
+		System.out.println(msg);
+	}
+
+}
