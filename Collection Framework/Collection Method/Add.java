0135b540dfde664a5186a938d8dcaadc5255648b
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-02-14 11:37:48 +0530

    Update Add.java

--- Collection Framework/Collection Method/Add.java
+++ Collection Framework/Collection Method/Add.java
@@ -1,1 +1,13 @@
+//public boolean add(Object obj)
+//It can be used to add the provided element to the Collection object , if the element is added successfully then the add() method will return true value, 
+//if the element is not added to the Collection then the add() method will return false value.
 
+import java.util.HashSet;
+
+class Add {
+    public static void main(String[] args) {
+        HashSet hashSet = new HashSet();
+        System.out.println(hashSet.add("Rhutik"));
+        System.out.println(hashSet.add("Rhutik"));
+    }
+}
