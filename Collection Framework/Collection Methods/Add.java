2fa124042a281d92e0a5bda0006c9c525ab950bd
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-02-14 11:14:54 +0530

    Update Add.java

--- Collection Framework/Collection Methods/Add.java
+++ Collection Framework/Collection Methods/Add.java
@@ -2,16 +2,12 @@
 //It can be used to add the provided element to the Collection object , if the element is added successfully then the add() method will return true value, 
 //if the element is not added to the Collection then the add() method will return false value.
 
-
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 
-
-class Add{
-   public static void main(String[] args) {
-       HashSet hashSet = new HashSet();
-       System.out.println(hashSet.add("AAA"));
-       System.out.println(hashSet.add("AAA"));
-   }
+class Add {
+    public static void main(String[] args) {
+        HashSet hashSet = new HashSet();
+        System.out.println(hashSet.add("Rhutik"));
+        System.out.println(hashSet.add("Rhutik"));
+    }
 }
