19882498cd6b65ec331fb3a1c0863e040996d474
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-03-12 13:34:34 +0530

    Create LinkedListDemo.java

---
+++ Collection Framework/LinkedList/LinkedListDemo.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+	public static void main(String[] args) {
+		LinkedList<Integer> l=new LinkedList<Integer>();
+		l.add(15);
+		l.add(25);
+		l.add(45);
+		l.add(35);
+		l.add(55);
+		
+		System.out.println(l);
+
+	}
+
+}
