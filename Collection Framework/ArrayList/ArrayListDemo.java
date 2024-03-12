9236a04038877ba6f3b85c16d0be5de9fb658f81
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-03-12 13:29:29 +0530

    Create ArrayListDemo.java

---
+++ Collection Framework/ArrayList/ArrayListDemo.java
@@ -0,0 +1,18 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+	public static void main(String[] args) {
+		ArrayList<String> cityNames=new ArrayList<String>();
+		cityNames.add("Pune");
+		cityNames.add("Mumbai");
+		cityNames.add("Nashik");
+		cityNames.add("Nagar");
+		cityNames.add("Hyderabad");
+
+
+		System.out.println(cityNames);
+	}
+
+}
