029407fc7f3b548d8dc96b8b8a4f99851cc3eed8
Rhutik  Anasharam Jagtap <118281847+RhutikJagtap@users.noreply.github.com>
2024-04-15 19:03:02 +0530

    Create BubbleSort.java

---
+++ Sorting Algorithms/BubbleSort.java
@@ -0,0 +1,34 @@
+// large elements come to the end of array by swapping with adjacent elements.
+//time complexity Big O(n^2)
+public class BubbleSort {
+
+    // function for bubbleSort
+    public static void bubbleSort(int arr[]) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+
+            }
+        }
+    }
+
+    // function for printing Array
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 5, 4, 1, 3, 2 }; // n-1 turns =5-1=4turns
+
+        bubbleSort(arr);
+        printArr(arr);
+    }
+
+}
